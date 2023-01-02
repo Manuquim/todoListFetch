de5f58f2536b808315076c186e4dfc94354883e7
Manuquim <manuelcabreraosorio11@gmail.com>
2023-01-02 11:25:09 +0000

    commit on 020123

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = () =>{
+	const [texto,setTexto]=useState("");
+	const [lista,setLista]=useState([]);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="container">
+			<h1>TO-DO List</h1>
+			<ul>
+				<li id="first">
+					<input type="text"
+        			onChange={(e) => setTexto(e.target.value)}
+        			value={texto}
+       			 	placeholder="Please, enter new task!!!"
+					onKeyDown={(e) => 
+						{if(e.key=='Enter'&& texto!="")
+							{
+								setLista(lista.concat(texto));
+								setTexto("");
+							}
+						}
+					}	
+					/>
+				</li>
+				{lista.map((i,item) => 
+					<li>{i} <i class="far fa-trash-alt" 
+					onClick={()=>setLista(lista.filter((x,indexA)=>item!=indexA))}> 
+					</i></li>)
+				}
+			</ul>
+			<div id="numberItems">{lista.length} items</div>
+		</div> 
 	);
 };
 
